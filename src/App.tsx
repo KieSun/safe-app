5bddddc685710c1d52d44bcec677ad838db40393
KieSun <zx597813039@gmail.com>
2024-01-07 18:20:24 +0800

    feat: send tx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,42 @@ import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
 import { ConnectWallet } from '@thirdweb-dev/react'
 import { useAddress } from '@thirdweb-dev/react'
 import { fetchData } from './services/project'
+import { ethers } from 'ethers'
+// import abiCoder from 'web3-eth-abi'
+import { ERC_20_ABI } from './abis/erc20'
+const abiCoder = require('web3-eth-abi')
+
+function encodeTxData(method: any, recipient: string, amount: string): string {
+  const coder = abiCoder as any
+  return coder.encodeFunctionCall(method, [recipient, amount])
+}
 
 const SafeApp = (): React.ReactElement => {
   const { sdk, safe } = useSafeAppsSDK()
   const address = useAddress()
   const [data, setData] = useState<any>(null)
 
-  const submitTx = useCallback(async () => {
+  const handleTransfer = useCallback(async () => {
     try {
       const { safeTxHash } = await sdk.txs.send({
         txs: [
+          // usdc transfer
           {
-            to: safe.safeAddress,
+            // get from fetchBalance tokeninfo address
+            to: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
             value: '0',
+            data: encodeTxData(
+              ERC_20_ABI.transfer,
+              '0x31aB1A1F559e0A025f8523C64c9988d5850B83aC',
+              // parse units with token decimals
+              ethers.utils.parseUnits('0.1', 6).toString(),
+            ),
+          },
+          // eth transfer
+          {
+            to: '0x31aB1A1F559e0A025f8523C64c9988d5850B83aC',
+            // parse units with eth decimals
+            value: ethers.utils.parseEther('0.1').toString(),
             data: '0x',
           },
         ],
@@ -27,13 +50,20 @@ const SafeApp = (): React.ReactElement => {
     } catch (e) {
       console.error(e)
     }
-  }, [safe, sdk])
+  }, [sdk.txs])
+
+  const fetchBalance = useCallback(async () => {
+    const data = await sdk.safe.experimental_getBalances()
+    console.log(data)
+  }, [sdk.safe])
 
   useEffect(() => {
     if (address) {
       fetchData(address).then((res) => setData(res))
+      fetchBalance()
+      // 0x0013c41da98f5daa0eed756d059757205dd1238ee8fc76c8c227e8d539abbc8a
     }
-  }, [address])
+  }, [address, fetchBalance])
 
   return (
     <Container>
@@ -42,7 +72,7 @@ const SafeApp = (): React.ReactElement => {
           <Typography variant="h3">Safe: {safe.safeAddress}</Typography>
         </Grid>
         <Grid item>
-          <Button variant="contained" color="primary" onClick={submitTx}>
+          <Button variant="contained" color="primary" onClick={handleTransfer}>
             Click to send a test transaction
           </Button>
         </Grid>
